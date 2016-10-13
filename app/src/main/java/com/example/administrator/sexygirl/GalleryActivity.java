653a7ca3dc30d8842b37214e24f6d3985290d810
eclipse4dk <547839603@qq.com>
2016-10-13 14:33:22 +0800

    gallery

--- app/src/main/java/com/example/administrator/sexygirl/GalleryActivity.java
+++ app/src/main/java/com/example/administrator/sexygirl/GalleryActivity.java
@@ -1,62 +1,55 @@
 package com.example.administrator.sexygirl;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import jp.wasabeef.glide.transformations.CropCircleTransformation;
 
-public class MainActivity extends AppCompatActivity {
+public class GalleryActivity extends AppCompatActivity {
 
-    private List<ClassifyBean.TngouBean> datas;
-    private ListView listview;
+    @BindView(R.id.gallery_listview)
+    ListView mGalleryListview;
+    private List<GalleryBean.TngouBean> datas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        listview = (ListView) findViewById(R.id.listview);
-    loadDataFromServer();
-        listview.setOnItemClickListener(listener);
+        setContentView(R.layout.activity_gallery);
+        ButterKnife.bind(this);
+        loadDataFromServer();
     }
 
-    private AdapterView.OnItemClickListener listener = new AdapterView.OnItemClickListener() {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            Intent intent = new Intent(MainActivity.this,GalleryActivity.class);
-            intent.putExtra("id",position);
-            startActivity(intent);
-        }
-    };
-
     private void loadDataFromServer() {
-        String url = "http://apis.baidu.com/tngou/gallery/classify";
-        SexyGirlRequest<ClassifyBean> request = new SexyGirlRequest<ClassifyBean>(url, ClassifyBean.class, ClassifyBeanListener, ErrorListener);
+        int id = getIntent().getIntExtra("id", 0);
+        String url = "http://apis.baidu.com/tngou/gallery/list?id=" + id + "&page=1&rows=20";
+        SexyGirlRequest<GalleryBean> request = new SexyGirlRequest<>(url, GalleryBean.class, mGalleryBeanListener, mErrorListener);
         NetworkManager.sendRequest(request);
     }
 
-    private Response.Listener<ClassifyBean> ClassifyBeanListener = new Response.Listener<ClassifyBean>() {
+    private Response.Listener<GalleryBean> mGalleryBeanListener = new Response.Listener<GalleryBean>() {
         @Override
-        public void onResponse(ClassifyBean response) {
+        public void onResponse(GalleryBean response) {
             datas = response.getTngou();
-            listview.setAdapter(adapter);
+            mGalleryListview.setAdapter(adapter);
         }
     };
 
-    private Response.ErrorListener ErrorListener = new Response.ErrorListener() {
+    private Response.ErrorListener mErrorListener = new Response.ErrorListener() {
         @Override
         public void onErrorResponse(VolleyError error) {
 
@@ -83,19 +76,23 @@ public class MainActivity extends AppCompatActivity {
         public View getView(int position, View convertView, ViewGroup parent) {
             ViewHolder holder = null;
             if(convertView==null) {
-                convertView = LayoutInflater.from(MainActivity.this).inflate(R.layout.list_item,null);
+                convertView = LayoutInflater.from(GalleryActivity.this).inflate(R.layout.gallerylist_item,null);
                 holder = new ViewHolder(convertView);
                 convertView.setTag(holder);
             }else {
                 holder = (ViewHolder) convertView.getTag();
             }
+            String url = "http://tnfs.tngou.net/image" + datas.get(position).getImg();
+            Glide.with(GalleryActivity.this).load(url).bitmapTransform(new CropCircleTransformation(GalleryActivity.this)).into(holder.image);
             holder.title.setText(datas.get(position).getTitle());
             return convertView;
         }
     };
 
     public class ViewHolder {
-        @BindView(R.id.title)
+        @BindView(R.id.gallery_list_item_image)
+        ImageView image;
+        @BindView(R.id.gallery_list_item_title)
         TextView title;
         public ViewHolder(View v) {
             ButterKnife.bind(this,v);
